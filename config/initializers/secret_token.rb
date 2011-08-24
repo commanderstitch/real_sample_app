5ab16ff57f21bbe39583f67c60dfbae2da3476a4
Benjamin Atwood <benjamin@orbitalimpact.com>
2011-08-24 13:31:49 -0400

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealSampleApp::Application.config.secret_token = '580ffa228268de5c94ea916499e87590ad515428c4b509a1ad9219031fabf9a3a485dd88686076a36f6a1e99a6adcd9df217c88f1648377e4357ff33f7fd545d'
