d6cf013693f7e3d38879f995f1b24b2a9d04d0be
Benjamin Atwood <benjamin@orbitalimpact.com>
2011-08-31 15:53:03 -0400

    added micropost model with user associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    @user.destroy
     flash[:success] = "User destroyed HAHAHAHAHA he's GONE history BYE BYE yes!! ahahaha!!'"
     redirect_to users_path
   end
@@ -63,8 +63,8 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-    user = User.find(params[:id])
-    redirect_to(root_path) if !current_user.admin? || current_user?(user)
+    @user = User.find(params[:id])
+    redirect_to(root_path) if !current_user.admin? || current_user?(@user)
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
 
+  has_many :microposts, :dependent => :destroy
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, :presence => true,
@@ -63,4 +65,3 @@ class User < ActiveRecord::Base
     Digest::SHA2.hexdigest(string)
   end
 end
-
