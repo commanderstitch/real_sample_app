5cd22a362e4b72c827b6e93accf76fbc22ff8fbf
Benjamin Atwood <benjamin@orbitalimpact.com>
2011-09-01 18:44:35 -0400

    user relationships(following)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,3 +19,15 @@ class Micropost < ActiveRecord::Base
 
   default_scope :order => 'microposts.created_at DESC'
 end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
