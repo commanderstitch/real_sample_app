b2b75bc599dddc6835da499cb4f3df836c6ad8f8
Benjamin Atwood <benjamin@orbitalimpact.com>
2011-09-01 16:10:39 -0400

    finished microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,19 +1,28 @@
 class MicropostsController < ApplicationController
 
   before_filter :authenticate
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
        flash[:success] = "CONGRADULATIONS! you have successfully posted a post ON THE PAGE!!"
       redirect_to root_path
-      else
+    else
+      @feed_items = []
       render 'pages/home'
       end
   end
 
   def destroy
+    @micropost.destroy
+     flash[:success] = "Goodbye useless post, i hated you anyway."
+    redirect_to root_path
+  end
+  private
 
-
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micorpost.user)
   end
 end
